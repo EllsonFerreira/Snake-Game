317052a59ded71069b44b430f3911cfde6146f08
EllsonFerreira <ellsonpjlvab@gmail.com>
2022-06-22 14:59:28 -0300

    correção de bug e finalização do projeto, faltando pontuação

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let canvas = document.getElementById('snake');
 let context = canvas.getContext('2d');
 let box = 32;
 let snake = [];
+
 snake[0] = {
     x: 8 * box,
     y: 8 * box
@@ -17,14 +18,14 @@ function criarBG() {
     context.fillRect(0, 0, 16 * box, 16* box);
 }
 
-function criarCobrinha() {
+function criarCobra() {
     for(i = 0; i < snake.length; i++) {
         context.fillStyle = 'black';
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-function drawFood() {
+function criarComida() {
     context.fillStyle = 'red';
     context.fillRect(food.x, food.y, box, box);
 }
@@ -40,23 +41,34 @@ function update(event) {
 
 function iniciarJogo() {
 
-    if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
-    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
-    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+    if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0
+    if(snake[0].x > 15 * box && direction == 'up') snake[0].x = 0
+    if(snake[0].x > 15 * box && direction == 'down') snake[0].x = 0
+
+    if(snake[0].x < 0  && direction == 'left') snake[0].x = 15 * box
+    if(snake[0].x < 0  && direction == 'up') snake[0].x = 15 * box
+    if(snake[0].x < 0  && direction == 'down') snake[0].x = 15 * box
+
+    if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0
+    if(snake[0].y > 15 * box && direction == 'right') snake[0].y = 0
+    if(snake[0].y > 15 * box && direction == 'left') snake[0].y = 0
+
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 15 * box
+    if(snake[0].y < 0 && direction == 'right') snake[0].y = 15 * box
+    if(snake[0].y < 0 && direction == 'left') snake[0].y = 15 * box
 
     for(i = 1; i < snake.length; i++) {
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
             clearInterval(jogo);
-            alert('Game Over... :(');
+            alert('Game Over!');
             
         }
     }
 
     criarBG();
-    criarCobrinha();
-    drawFood();
-
+    criarCobra();
+    criarComida();
+    
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
     
